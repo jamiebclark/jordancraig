90806a13497b038fd3f8be2844b86c0c03183063
Jamie from Desktop <jamiebclark@gmail.com>
2013-11-11 23:51:12 -0500

    Turned debugging back on for rolling out to web testing

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,6 +31,7 @@ App::uses('Controller', 'Controller');
  * @package		app.Controller
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
+ 
 class AppController extends Controller {
 	var $components = array(
 		'FormData.FormData',
