1a3fb357b2e48c1b1dcbcac2c7f5512eb052a6f4
Jamie from Desktop <jamiebclark@gmail.com>
2013-11-11 23:18:10 -0500

    Switched to production mode. Added error handler

--- app/Config/core.php
+++ app/Config/core.php
@@ -33,7 +33,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug', 0);
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
@@ -78,7 +78,7 @@
  */
 	Configure::write('Exception', array(
 		'handler' => 'ErrorHandler::handleException',
-		'renderer' => 'AppExceptionRenderer',
+		'renderer' => 'ExceptionRenderer',
 		'log' => true
 	));
 
